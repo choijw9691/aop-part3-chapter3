eaf155a0cd0c994f0d590ac73d182598db861170
choijiwoung <choijw969133@gmail.com>
2021-10-07 17:03:25 +0900

    20211007

--- app/src/main/java/com/example/aop_part3_chapter3/AlarmDisplayModel.kt
+++ app/src/main/java/com/example/aop_part3_chapter3/AlarmDisplayModel.kt
@@ -2,6 +2,7 @@ package com.example.aop_part3_chapter3
 
 import android.app.TimePickerDialog.OnTimeSetListener
 import android.content.Context
+import android.util.Log
 import android.widget.TimePicker
 import kotlin.math.min
 
@@ -17,6 +18,12 @@ data class AlarmDisplayModel(val hour: Int, val minute: Int, var onoffCheck: Boo
     val ampmText: String
         get() {
             return if (hour < 12) "AM" else "PM"
+        }
+
+    val onOffText: String
+        get() {
+            Log.d("getofftext",onoffCheck.toString())
+            return if (onoffCheck) "알람 끄기" else "알람 켜기"
         }
 
     fun makeDataForDB():String{
--- app/src/main/java/com/example/aop_part3_chapter3/MainActivity.kt
+++ app/src/main/java/com/example/aop_part3_chapter3/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aop_part3_chapter3
 
+import android.app.AlarmManager
 import android.app.PendingIntent
 import android.app.TimePickerDialog
 import android.content.Context
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     private val timeTextView by lazy { findViewById<TextView>(R.id.timeTextView) }
     private val ampmTextView by lazy { findViewById<TextView>(R.id.ampmTextView) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,7 +38,25 @@ class MainActivity : AppCompatActivity() {
         val timeTextView = sharedPreferences.getString("alarm", "09:00") ?: "09:00"
         val onoffCheck = sharedPreferences.getBoolean("onOff", false)
         var array = timeTextView.split(":")
-        saveAlarmModel(array[0].toInt(), array[1].toInt(), onoffCheck)
+        val model = saveAlarmModel(array[0].toInt(), array[1].toInt(), onoffCheck)
+        val pendingIntent = PendingIntent.getBroadcast(
+            this,
+            1000,
+            Intent(this, AlarmReceiver::class.java),
+            PendingIntent.FLAG_NO_CREATE
+        )
+
+        if ((pendingIntent == null) and model.onoffCheck) {
+            // 알람은 꺼져있는데, 데이터는 켜저있는 경우
+            model.onoffCheck = false
+
+        } else if ((pendingIntent != null) and model.onoffCheck.not()) {
+            // 알람은 켜져있는데, 데이터는 꺼져있는 경우
+            // 알람을 취소함
+            pendingIntent.cancel()
+        }
+
+        renderView(model)
 
     }
 
@@ -45,6 +65,52 @@ class MainActivity : AppCompatActivity() {
 
         var onoffButton = findViewById<Button>(R.id.onOffButton)
         onoffButton.setOnClickListener {
+            val model = it.tag as? AlarmDisplayModel ?: return@setOnClickListener
+            val newModel = saveAlarmModel(model.hour, model.minute, model.onoffCheck.not())
+            renderView(newModel)
+
+            if (newModel.onoffCheck) {
+
+                val calendar = Calendar.getInstance().apply {
+                    set(Calendar.HOUR_OF_DAY, newModel.hour)
+                    set(Calendar.MINUTE, newModel.minute)
+
+                    if (before(Calendar.getInstance())) {
+                        add(Calendar.DATE, 1)
+                    }
+                }
+
+                val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                val pendingIntent = PendingIntent.getBroadcast(
+                    this, 1000,
+                    Intent(this, AlarmReceiver::class.java), PendingIntent.FLAG_UPDATE_CURRENT
+                )
+                alarmManager.setInexactRepeating(
+                    AlarmManager.RTC_WAKEUP,
+                    calendar.timeInMillis,
+                    AlarmManager.INTERVAL_DAY,
+                    pendingIntent
+
+                )
+
+
+            } else {
+                val pendingIntent = PendingIntent.getBroadcast(
+                    this,
+                    1000,
+                    Intent(this, AlarmReceiver::class.java),
+                    PendingIntent.FLAG_NO_CREATE
+                )
+                pendingIntent?.cancel()
+            }
+
+
+            val pendingIntent = PendingIntent.getBroadcast(
+                this,
+                1000,
+                Intent(this, AlarmReceiver::class.java),
+                PendingIntent.FLAG_NO_CREATE
+            )
 
 
         }
@@ -57,18 +123,16 @@ class MainActivity : AppCompatActivity() {
 
             val calendar = Calendar.getInstance()
             TimePickerDialog(this, { picker, hour, minute ->
-                val model:AlarmDisplayModel = saveAlarmModel(hour, minute, true)
-                timeTextView.text = model.timeText
-                ampmTextView.text = model.ampmText
+                val model: AlarmDisplayModel = saveAlarmModel(hour, minute, false)
 
+                renderView(model)
                 val pendingIntent = PendingIntent.getBroadcast(
                     this,
                     1000,
                     Intent(this, AlarmReceiver::class.java),
                     PendingIntent.FLAG_NO_CREATE
                 )
-
-                pendingIntent.cancel()
+                pendingIntent?.cancel()
 
 
             }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show()
@@ -78,7 +142,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun saveAlarmModel(hour: Int, minute: Int, onoffCheck: Boolean): AlarmDisplayModel {
 
-        val model = AlarmDisplayModel(hour, minute, true)
+        val model = AlarmDisplayModel(hour, minute, onoffCheck)
 
 
         val sharedPreferences = getSharedPreferences("time", Context.MODE_PRIVATE)
@@ -89,18 +153,25 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
 
-        val pendingIntent = PendingIntent.getBroadcast(
-            this,
-            1000,
-            Intent(this, AlarmReceiver::class.java),
-            PendingIntent.FLAG_NO_CREATE
-        )
-        if ((pendingIntent == null) and model.onoffCheck) {
-            model.onoffCheck = false
-        } else if ((pendingIntent != null) and model.onoffCheck.not()) {
-            pendingIntent.cancel()
-        }
+
         return model
     }
+
+    private fun renderView(model: AlarmDisplayModel) {
+        findViewById<TextView>(R.id.ampmTextView).apply {
+            text = model.ampmText
+        }
+
+        findViewById<TextView>(R.id.timeTextView).apply {
+            text = model.timeText
+        }
+
+        findViewById<Button>(R.id.onOffButton).apply {
+            text = model.onOffText
+            tag = model
+        }
+
+    }
+
 
 }
